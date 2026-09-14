2af498c594b293e0dd7c24ccb867a5bc8e0b5962
arshiaahmadian-coder <arshiaahmadiancoder@gmail.com>
2026-09-14 15:06:55 +0330

    button animation

--- PedalPedal/Assets/Scripts/UI/CustomButton.cs
+++ PedalPedal/Assets/Scripts/UI/CustomButton.cs
@@ -6,16 +6,21 @@ public class CustomButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
 {
     [SerializeField] private bool OnButtonDown;
     [SerializeField] private UnityEvent onClick;
+    [SerializeField] private Animator animator;
 
     public void OnPointerDown(PointerEventData eventData)
     {
         if (!OnButtonDown) return;
         onClick?.Invoke();
+        if (animator != null)
+            animator.SetTrigger("Action");
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         if (OnButtonDown) return;
         onClick?.Invoke();
+        if (animator != null)
+            animator.SetTrigger("Action");
     }
 }
